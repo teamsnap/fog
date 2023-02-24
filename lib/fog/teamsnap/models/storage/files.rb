19968a6a27cab5af2fb51e5da1ebc4c5f9d38238
Tim D <88800750+timdsnap@users.noreply.github.com>
2023-02-24 17:28:05 -0600

    feat: write to GCS, read from GCS with CF fallback

--- lib/fog/teamsnap/models/storage/files.rb
+++ lib/fog/teamsnap/models/storage/files.rb
@@ -26,22 +26,27 @@ module Fog
         end
 
         def head(key, options={})
-          google.head(key, options)
-          #rackspace.head(key, options)
+          google.head(key, options) || rackspace.head(key, options)
         end
 
         def new(attributes = {})
-          google.new(attributes)
-          #rackspace.new(attributes)
+          creating = attributes.delete(:creating)
+          objs = google.new(attributes)
+
+          return objs if creating
+
+          objs.public_url #will throw the Excon::Error::NotFound error if not in GCS
+          objs
+        rescue Excon::Error::NotFound
+          rackspace.new(attributes)
         end
 
         def get(key, &block)
-          google.get(key, &block)
-          #rackspace.get(key, &block)
+          google.get(key, &block) || rackspace.get(key, &block)
         end
 
         def create(attributes = {})
-          object = new(attributes)
+          object = new(attributes.merge({creating: true}))
           object.save
           object
         end
